819682e109737b76261122a4fb5456f6cfba0989
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2010-02-24 20:46:11 +0000

    updated leap year

--- leapYear.rb
+++ leapYear.rb
@@ -7,3 +7,4 @@ Every fourth year is a leap year.			2004, 2008, and 2012 are leap years.	Every
 puts 'welcome to the leap year calculator'
 puts 'give me the top and bottom range of years'
 puts 'and I\'ll tell you the leap years between them'
+
