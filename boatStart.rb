8426c3fbf89316595860969ead34d7ac5ba03ee9
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2010-03-04 13:44:55 +0000

    added details to boat

--- boatStart.rb
+++ boatStart.rb
@@ -1,12 +1,17 @@
 # class example in Ruby
+# adding variables values to initilize
+# means they are used with no-argument calls to .new
 
 class Boat
-  def initialize(name,length,colour)
+  def initialize(name="Sunrise",length=20,colour="blue")
     @name = name
     @length = length
     @colour = colour
   end
 end
 
-boat = Boat.new("Sunrise",20, "blue")
+boat = Boat.new
 puts boat.inspect.to_s
+
+boat2 = Boat.new("Sunset Song", 22, colour="yellow")
+puts boat2.inspect.to_s
