57271905ea9cca819b552e718b26a3eeb8592ff4
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2010-04-20 09:52:40 +0100

    commented write file example

--- writeFile.rb
+++ writeFile.rb
@@ -1,5 +1,10 @@
 # simple example of writing to file
 
+# this time we use the second parameter to signal that
+# we want to write to the file, default is read 'r'
+# we can also use 'r+' for read-write
+# be aware that 'w' will overwrite anything in the file
+
 file = File.open("myfile.txt", "w")
 # with file open we need to put some content in there
 
