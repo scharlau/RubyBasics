59b42f617a3aa6badab86962eec1a9e942555988
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2011-01-28 16:45:32 +0000

    changed to class file solutions

--- 02/numbers.rb
+++
@@ -1,8 +0,0 @@
-module Numbers
-  
-
-  @myNum = 23
-  @hours_in_year = 33
-  $minutes_in_decade= 44
- 
-end
--- 02/numbers_spec.rb
+++ 02/numbers_spec.rb
@@ -1,27 +1,18 @@
 # spec file for ruby tests
-# write your code in the section noted
+require 'numbers'
 
 describe "some ruby numbers" do
   
-  before(:all) do
-    # put your own code here
-    @myNum = 23
-    @hours_in_year = 24*365
-    @minutes_in_decade = 60*@hours_in_year*10
-    
-    # you should not write anything below this line
-  end
-  
   it "should tell me my number" do
-    @myNum.should == 23
+    Numbers.new.myNum.should == 24
   end
   
   it "should tell me how many hours are in a year" do
-    @hours_in_year.should == 8760
+    Numbers.new.hours_in_year.should == 8760
   end
   
   it "should tell me how many minutes are in a decade" do
-     @minutes_in_decade.should == 5256000
+     Numbers.new.minutes_in_decade.should == 5256000
   end
   
   # extra exercise to work out
@@ -31,9 +22,9 @@ describe "some ruby numbers" do
   
   after(:all) do
     puts # just a blank line to separate output
-    puts "myNum is " + @myNum.to_s
-    puts "There are #{@hours_in_year} hours in a year"
-    puts "There are #{@minutes_in_decade} minutes in a decade"
+    puts "myNum is " + Numbers.new.myNum.to_s
+    puts "There are #{Numbers.new.hours_in_year} hours in a year"
+    puts "There are #{Numbers.new.minutes_in_decade} minutes in a decade"
   end
   
   
