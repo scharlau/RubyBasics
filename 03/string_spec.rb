5f1f9959f8a311490857bf045eea510ffc925cb5
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2011-02-08 17:18:12 +0000

    added tests to string_spec

--- 03/string_spec.rb
+++ 03/string_spec.rb
@@ -1,31 +1,27 @@
 # spec file for ruby tests
 # find more on string at http://ruby-doc.org/core/
-# write your code in the section noted
+require 'hello'
+
 
 describe "some ruby string methods" do
-  
-  before(:all) do
-    #put your own code here
-    
-    
-    
-    # you should not write anything below this line
+it "should greet me" do
+    Hello.new.greeting == "hello there"
   end
-  
+    
   it "should tell me how many characters there are" do
-    pending
+    Hello.new.number("hello world") == 11
   end
   
   it "should reverse the order of the characters" do
-     pending
+     Hello.new.reverse("hello world") == "dlrow olleh"
   end
   
   it "should capitalise everything" do
-     pending
+     Hello.new.caps("hello world") == "HELLO WORLD"
   end
   
   it "should repeat the word" do
-     pending
+     Hello.new.repeat("hello world") == "hello worldhello world"
   end
   
 end
