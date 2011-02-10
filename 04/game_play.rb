d949d1753c3f2fc77b4c8222872000ee4de8d319
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2011-02-10 08:33:09 +0000

    extended guess example

---
+++ 04/game_play.rb
@@ -0,0 +1,18 @@
+# test for ruby app using modules
+
+require 'game'
+module Game
+
+
+guess = Game::Guess.new(STDOUT)
+guess.start
+guesses = 3
+while guesses >0 
+my_guess = gets.chomp
+guesses = guesses -1
+guess.compare(my_guess)
+end
+
+puts "my number was #{guess.the_number} "
+
+end
