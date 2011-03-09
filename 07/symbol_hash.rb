e5a45d2cfc53f6c160f83f97377e3d8e74d0f943
Bruce Scharlau <b.scharlau@abdn.ac.uk>
2011-03-09 07:18:57 +0000

    updated symbol_hash

--- 07/symbol_hash.rb
+++ 07/symbol_hash.rb
@@ -11,8 +11,8 @@ puts 'select the coast you like:'
 puts 'choose 1 for glasgow, 2 for edinburgh, 3 for inverness and 4 for stornoway'
 choice = gets.chomp.to_i
 case(choice)
-	when 1: puts city_hash[:glasgow]
-	when 2: puts city_hash[:edinburgh]
-	when 3: puts city_hash[:inverness]
-	when 4: puts city_hash[:stornoway]
+	when 1 then puts city_hash[:glasgow]
+	when 2 then puts city_hash[:edinburgh]
+	when 3 then puts city_hash[:inverness]
+	when 4 then puts city_hash[:stornoway]
 end
